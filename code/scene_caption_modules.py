00e88b392079f7c3d2a8c27c42f3ca914ea2055c
junhanjeong <junhan135790@gmail.com>
2025-01-27 05:50:15 +0000

    feat: implement scene_caption_InternVideo2 #3

--- code/scene_caption_modules.py
+++ code/scene_caption_modules.py
@@ -14,13 +14,8 @@ import os
 
 import decord
 import torch
-import yaml
+from audio_utils import transcribe_audio
 from googletrans import Translator
-from scene_utils import (
-    read_timestamps_from_txt,
-    save_all_video_scenes_by_timestamps,
-    save_timestamps_to_txt,
-)
 from specific_model_utils.InternVideo2_utils import load_video
 from transformers import AutoModel, AutoTokenizer
 
@@ -28,6 +23,15 @@ decord.bridge.set_bridge("torch")
 
 
 def initialize_model(model_path="OpenGVLab/InternVideo2-Chat-8B"):
+    """
+    model_path를 받아 모델과 tokenizer를 반환하는 함수
+
+    Args:
+        model_path (str): 모델 경로
+
+    Returns:
+        model, tokenizer
+    """
     tokenizer = AutoTokenizer.from_pretrained(
         model_path, trust_remote_code=True, use_fast=False
     )
@@ -39,101 +43,84 @@ def initialize_model(model_path="OpenGVLab/InternVideo2-Chat-8B"):
 
 
 def scene_caption_InternVideo2(
-    video_dir,
-    output_directory,
-    timestamp_file,
-    folder_name,
     model_path,
-    generation_config,
+    scene_path,
     prompt,
-    final_output_path,
-    use_audio,
-    output_json_path,
+    translator,
+    generation_config,
+    use_audio_for_prompt,
+    scene_info_json_file_path=None,  # 오디오 스크립트 정보 포함
 ):
-    model, tokenizer = initialize_model(model_path)
+    """
+    1개의 scene에 대한 캡션을 InternVideo2 모델을 사용하여 생성하는 함수
 
+    Args:
+        model_path (str): 모델 경로
+        scene_path (str): scene 경로
+        prompt (dict): prompt 정보
+        translator (googletrans.Translator): 번역기
+        generation_config (dict): 생성 설정
+        use_audio_for_prompt (bool): VideoLM으로 추론할 때, 오디오자막을 프롬프트에 넣어줄지 여부
+        scene_info_json_file_path (str): scene 정보 json 파일 경로 (해당 Json에는 오디오 스크립트 정보 포함되어 있음)
+
+        만약, use_audio_for_prompt가 True이고 scene_info_json_file_path가 None이면, whisper 모델을 사용하여 오디오 스크립트 추출
+        만약, use_audio_for_prompt가 True이고 scene_info_json_file_path가 있으면, 해당 경로에서 오디오 스크립트 추출
+
+    Returns:
+        result (dict): 캡션 결과
+    """
+    model, tokenizer = initialize_model(model_path)
     translator = Translator()
 
-    # 최종 JSON 데이터 구조 생성
-    final_json_data = []
+    scene_tensor = load_video(scene_path, num_segments=8, return_msg=False)
+    scene_tensor = scene_tensor.to(model.device)
 
-    scene_timestamps = read_timestamps_from_txt(timestamp_file)
+    # scene_name 추출 (audio_name이랑 같음 - {video_id}_{start:.3f}_{end:.3f}_{i + 1:03d})
+    scene_name = os.path.basename(scene_path).split(".")[0]
+    video_id, start, end, scene_id = scene_name.split("_")
 
-    for video_id, timestamps in scene_timestamps.items():
-        for i, (start, end) in enumerate(timestamps):
-            clip_name = f"{video_id}_{start:.3f}_{end:.3f}_{i + 1:03d}.mp4"
-            clip_path = os.path.join(output_directory, clip_name)
-            video_tensor = load_video(clip_path, num_segments=8, return_msg=False)
-            video_tensor = video_tensor.to(model.device)
+    if use_audio_for_prompt:
+        if scene_info_json_file_path:
+            audio_path = os.path.join(scene_info_json_file_path, scene_name + ".wav")
+            with open(audio_path, "r") as f:
+                audio_text = json.load(f)[video_id][int(scene_id) - 1]["audio_text"]
+        else:
+            # STT 모델인 Whisper를 불러옴
+            import whisper
 
-            if use_audio:
-                with open(output_json_path, "r") as f:
-                    scripts = json.load(f)
-                audio_text = scripts[video_id][i]["audio_text"]
+            whisper_model = whisper.load_model("large-v3")
+            audio_text = transcribe_audio(scene_path, whisper_model)
 
-                # 비디오 클립 로드 및 모델 처리
+    prompt = (
+        prompt["clip_prompt_template"] + f"\n[script]: {audio_text}"
+        if use_audio_for_prompt
+        else prompt["clip_prompt_template"]
+    )
 
-            prompt = (
-                prompt["clip_prompt_template"] + f"\n[script]: {audio_text}"
-                if use_audio
-                else prompt["clip_prompt_template"]
-            )
+    chat_history = []
+    response, chat_history = model.chat(
+        tokenizer,
+        "",
+        prompt,
+        media_type="video",
+        media_tensor=scene_tensor,
+        chat_history=chat_history,
+        return_history=True,
+        generation_config=generation_config,
+    )
 
-            chat_history = []
-            response, chat_history = model.chat(
-                tokenizer,
-                "",
-                prompt,
-                media_type="video",
-                media_tensor=video_tensor,
-                chat_history=chat_history,
-                return_history=True,
-                generation_config=generation_config,
-            )
+    translated_description = translator.translate(response, src="en", dest="ko").text
 
-            translated_description = translator.translate(
-                response, src="en", dest="ko"
-            ).text
-
-            final_json_data.append(
-                {
-                    "video_id": video_id,
-                    "start_time": start,
-                    "end_time": end,
-                    "clip_id": f"{video_id}_{start}_{end}_{i + 1:03d}",
-                    "caption": response,
-                    "caption_ko": translated_description,
-                }
-            )
-
-    with open(final_output_path, "w", encoding="utf-8") as json_file:
-        json.dump(final_json_data, json_file, ensure_ascii=False, indent=4)
-
-    print(f"All outputs have been saved to {final_output_path}.")
+    result = {
+        "video_id": video_id,
+        "start_time": start,
+        "end_time": end,
+        "clip_id": f"{video_id}_{start}_{end}_{scene_id}",
+        "caption": response,
+        "caption_ko": translated_description,
+    }
+    return result
 
 
 if __name__ == "__main__":
-    # Load configuration from YAML file
-    with open("config/config.yaml", "r") as config_file:
-        config = yaml.safe_load(config_file)
-
-    video_dir = config["video_dir"]
-    output_directory = config["clip_output_directory"]  # full_clip저장할곳
-    timestamp_file = config["timestamp_file"]
-    folder_name = config["output_folder"]  # script
-    model_path = config["model_path"]
-    generation_config = config["generation_config"]
-    prompt = config["prompt"]
-    final_output_path = config["final_output"]
-
-    # Output folder setup
-    os.makedirs(folder_name, exist_ok=True)
-
-    # Get video files
-    video_files = [file for file in os.listdir(video_dir) if file.endswith(".mp4")]
-    print(video_files)
-    save_timestamps_to_txt(video_dir, timestamp_file, threshold=30.0, min_scene_len=2)
-    # 동영상 클립 분할 실행
-    save_all_video_scenes_by_timestamps(video_dir, output_directory, timestamp_file)
-    # 5개만 우선 테스트해봅니다.
-    # video_files = video_files[:2]
+    pass
