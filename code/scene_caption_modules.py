19b609bc2f6b635c367edf30ae5ebfae1ef49288
junhanjeong <kevin1579@naver.com>
2025-01-29 14:49:12 +0900

    feat: add InternVideo2_5_Chat modules #3

--- code/scene_caption_modules.py
+++ code/scene_caption_modules.py
@@ -18,6 +18,7 @@ from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
 
 from .audio_utils import transcribe_audio
+from .specific_model_utils.InternVideo2_5_Chat_utils import DescriptionGenerator
 from .specific_model_utils.InternVideo2_utils import load_video
 from .specific_model_utils.LlavaVideo_utils import (
     get_video_and_input_ids,
@@ -49,6 +50,10 @@ def initialize_model(model_path="OpenGVLab/InternVideo2-Chat-8B"):
     # LlavaVideo 모델에서는 자체적으로 tokenizer, model을 불러옴
     elif model_path == "lmms-lab/LLaVA-Video-7B-Qwen2":
         tokenizer, model, image_processor, max_length = load_llava_video_model()
+
+    elif model_path == "OpenGVLab/InternVideo2_5_Chat_8B":
+        generator = DescriptionGenerator(model_path=model_path)
+        return generator, None, None
 
     return model, tokenizer, image_processor
 
@@ -195,6 +200,69 @@ def single_scene_caption_LlavaVideo(
     return result
 
 
+def single_scene_caption_InternVideo2_5_Chat(
+    generator,
+    scene_path,
+    prompt,
+    use_audio_for_prompt,
+    mono_audio_folder,
+    scene_info_json_file_path=None,  # 오디오 스크립트 정보 포함
+):
+    """
+    1개의 scene에 대한 캡션을 InternVideo2 모델을 사용하여 생성하는 함수
+
+    Args:
+        generator (DescriptionGenerator): 모델
+        scene_path (str): scene 경로
+        prompt (dict): prompt 정보
+        use_audio_for_prompt (bool): VideoLM으로 추론할 때, 오디오자막을 프롬프트에 넣어줄지 여부
+        mono_audio_folder (str): 모노 오디오 폴더 경로
+        scene_info_json_file_path (str): scene 정보 json 파일 경로 (해당 Json에는 오디오 스크립트 정보 포함되어 있음)
+
+        만약, use_audio_for_prompt가 True이고 scene_info_json_file_path가 None이면, whisper 모델을 사용하여 오디오 스크립트 추출
+        만약, use_audio_for_prompt가 True이고 scene_info_json_file_path가 있으면, 해당 경로에서 오디오 스크립트 추출
+
+    Returns:
+        result (dict): 캡션 결과
+    """
+    translator = Translator()
+
+    # scene_name 추출 (audio_name이랑 같음 - {video_id}_{start:.3f}_{end:.3f}_{i + 1:03d})
+    scene_name = os.path.basename(scene_path)[: -len(".mp4")]
+    video_id, start, end, scene_id = scene_name.split("_")
+
+    if use_audio_for_prompt:
+        mono_audio_path = os.path.join(mono_audio_folder, scene_name + ".wav")
+        if scene_info_json_file_path:
+            with open(scene_info_json_file_path, "r") as f:
+                audio_text = json.load(f)[video_id][int(scene_id) - 1]["audio_text"]
+        else:
+            # STT 모델인 Whisper를 불러옴
+            import whisper
+
+            whisper_model = whisper.load_model("large-v3")
+            audio_text = transcribe_audio(mono_audio_path, whisper_model)
+
+        prompt += f"\n[script]: {audio_text}"
+
+    # transformers_logger.setLevel(logging.ERROR)  # 경고 메시지 무시 (해당 구간만 적용)
+    response = generator.describe_scene(scene_path, prompt, num_segments=32)
+    # transformers_logger.setLevel(logging.WARNING)  # 복구
+
+    translated_description = translator.translate(response, src="en", dest="ko").text
+
+    result = {
+        "video_id": video_id,
+        "start_time": start,
+        "end_time": end,
+        "clip_id": f"{video_id}_{start}_{end}_{scene_id}",
+        "scene_path": scene_path,
+        "caption": response,
+        "caption_ko": translated_description,
+    }
+    return result
+
+
 def single_scene_caption(
     model_path,
     model,
@@ -246,6 +314,15 @@ def single_scene_caption(
             tokenizer,
             image_processor,
             max_frames_num,
+            scene_path,
+            prompt,
+            use_audio_for_prompt,
+            mono_audio_folder,
+            scene_info_json_file_path,
+        )
+    elif model_path == "OpenGVLab/InternVideo2_5_Chat_8B":
+        return single_scene_caption_InternVideo2_5_Chat(
+            model,
             scene_path,
             prompt,
             use_audio_for_prompt,
