e9fcbc00c21d1030d384c03add15c5032eace27b
junhanjeong <kevin1579@naver.com>
2025-01-31 18:50:56 +0900

    fix: modify download sh evalScene

--- download_evalSceneCaption_scenes.sh
+++ download_evalSceneCaption_scenes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl "https://drive.usercontent.google.com/download?id=1MdAXzmJBvv1y1Zbf8hZfCYBq28lTdJVG&confirm=xxx" -o "scenes_evalCaption.zip"
+curl "https://drive.usercontent.google.com/download?id=1-ZPEtb9uzk7IbQKkSF918TKyVoQ4EFVt&confirm=xxx" -o "scenes_evalCaption.zip"
 
 unzip scenes_evalCaption.zip
 rm -rf scenes_evalCaption.zip
