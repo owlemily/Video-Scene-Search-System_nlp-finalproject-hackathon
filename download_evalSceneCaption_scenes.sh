e94d1e1f7551662ae0d923a8b438990470eb2bba
junhanjeong <kevin1579@naver.com>
2025-01-31 18:33:04 +0900

    fix: modify download evalSceneCaption dataset scripts

--- download_evalSceneCaption_scenes.sh
+++ download_evalSceneCaption_scenes.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-curl "https://drive.usercontent.google.com/download?id=1zlYhpT1UtYeA3zbIAE1pNxIb9yZTrMbM&confirm=xxx" -o "test_dataset_scene_79.zip"
+curl "https://drive.usercontent.google.com/download?id=1zLmHee31FQf2x1AFtMBB8BSUgaQEoW3O&confirm=xxx" -o "scenes_evalCaption.zip"
 
-unzip test_dataset_scene_79.zip
-rm -rf test_dataset_scene_79.zip
-mv test_dataset_scene scenes_evalCaption
+unzip scenes_evalCaption.zip
+rm -rf scenes_evalCaption.zip
